19d4bd53e4a562a4f2f2236f63200e8406d262ea
Sebastian Bickerle <sebastian.bickerle@lissi.id>
2024-06-10 16:17:45 +0200

    removed unused code
    Signed-off-by: Sebastian Bickerle <sebastian.bickerle@lissi.id>

--- tests/SD-JWT-Tests/DisclosureTests.cs
+++ tests/SD-JWT-Tests/DisclosureTests.cs
@@ -4,8 +4,8 @@ namespace SD_JWT_Tests;
 
 public class DisclosureTests
 {
-    private const string SerialisedDisclosure = "WyJfMjZiYzRMVC1hYzZxMktJNmNCVzVlcyIsImZhbWlseV9uYW1lIiwiTcO2Yml1cyJd";
-    private readonly Disclosure DeserialisedDisclosure = 
+    private readonly string _serialisedDisclosure = "WyJfMjZiYzRMVC1hYzZxMktJNmNCVzVlcyIsImZhbWlseV9uYW1lIiwiTcO2Yml1cyJd";
+    private readonly Disclosure _deserialisedDisclosure = 
         new("family_name", "Möbius") { Salt = "_26bc4LT-ac6q2KI6cBW5es" };
 
     [SetUp]
@@ -16,23 +16,23 @@ public class DisclosureTests
     [Test]
     public void CanSerializeDisclosure()
     {
-        Assert.That(SerialisedDisclosure, Is.EqualTo(DeserialisedDisclosure.Serialize()));
+        Assert.That(_serialisedDisclosure, Is.EqualTo(_deserialisedDisclosure.Serialize()));
     }
 
     [Test]
     public void CanDeserializeDisclosure()
     {
-        Assert.That(DeserialisedDisclosure.Salt,  Is.EqualTo(Disclosure.Deserialize(SerialisedDisclosure).Salt));
-        Assert.That(DeserialisedDisclosure.Name,  Is.EqualTo(Disclosure.Deserialize(SerialisedDisclosure).Name));
-        Assert.That(DeserialisedDisclosure.Value,  Is.EqualTo(Disclosure.Deserialize(SerialisedDisclosure).Value.ToString()));
+        Assert.That(_deserialisedDisclosure.Salt,  Is.EqualTo(Disclosure.Deserialize(_serialisedDisclosure).Salt));
+        Assert.That(_deserialisedDisclosure.Name,  Is.EqualTo(Disclosure.Deserialize(_serialisedDisclosure).Name));
+        Assert.That(_deserialisedDisclosure.Value,  Is.EqualTo(Disclosure.Deserialize(_serialisedDisclosure).Value.ToString()));
     }
 
     [Test]
     public void CanComputeHash()
     {
-        const string expectedHash = "TZjouOTrBKEwUNjNDs9yeMzBoQn8FFLPaJjRRmAtwrM";
+        string expectedHash = "TZjouOTrBKEwUNjNDs9yeMzBoQn8FFLPaJjRRmAtwrM";
 
-        string actualHash = DeserialisedDisclosure.GetDigest();
+        string actualHash = _deserialisedDisclosure.GetDigest();
         
         Assert.That(expectedHash, Is.EqualTo(actualHash));
     }
